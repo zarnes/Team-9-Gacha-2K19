5c36c8abe05666aa4711b53de3b21a8a4474dfba
zarnes <zarnes@live.com>
2019-04-13 20:21:38 +0200

    Added pathfinding and fps counter

---
+++ Assets/Scripts/MoveOnClick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveOnClick : MonoBehaviour
+{
+    private Camera _cam;
+    private NavMeshAgent _agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cam = Camera.main;
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                _agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
