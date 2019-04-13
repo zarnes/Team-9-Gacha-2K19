5ae5018055e98da9e5c6794e3710e6a56a196af1
zarnes <zarnes@live.com>
2019-04-14 00:07:47 +0200

    Continued map into map

--- Assets/Scripts/RoomZoom.cs
+++ Assets/Scripts/RoomZoom.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RoomZoom : MonoBehaviour
 {
+    public Transform Player;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,32 @@ public class RoomZoom : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            StopAllCoroutines();
+            StartCoroutine(ScaleUp(5));
+        }
+    }
+
+    public IEnumerator ScaleUp(float duration)
+    {
+        float timeStart = Time.time;
+        float timeCurrent = timeStart;
+        float timeEnd = timeStart + duration;
+
+        Vector3 direction = Player.position - transform.position;
+        transform.localScale = Vector3.one;
+
+        while (transform.localScale.x < 10)
+        {
+            timeCurrent += Time.deltaTime;
+            float percentage = Mathf.InverseLerp(timeStart, timeEnd, timeCurrent);
+            float scale = Mathf.Lerp(1, 10, percentage);
+            transform.localScale = Vector3.one * scale;
+
+            Player.Translate(direction * Time.deltaTime);
+
+            yield return null;
+        }
     }
 }
