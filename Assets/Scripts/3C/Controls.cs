654cdc9093e664734ec3cbbe041c77175b5d01be
OlivierArgentieri <olivierargentieri@gmail.com>
2019-04-14 10:26:41 +0200

    [OAI] Update OlivierScene

--- Assets/Scripts/3C/Controls.cs
+++ Assets/Scripts/3C/Controls.cs
@@ -6,10 +6,13 @@ using UnityEngine.AI;
 public class Controls : MonoBehaviour
 {
     private NavMeshAgent m_nav_mesh_agent_;
+
+    private GameObject m_current_selected_object_;
     // Start is called before the first frame update
     void Start()
     {
-        InputManager.GetInstance().OnClickLeftMouseButton += OnMouseHandler;
+        InputManager.GetInstance().OnClickLeftMouseButton += OnClickLeftMouseHandler;
+        InputManager.GetInstance().OnClickRightMouseButton += OnClickRightMouseHandler;
         m_nav_mesh_agent_ = GetComponent<NavMeshAgent>();
     }
 
@@ -19,7 +22,7 @@ public class Controls : MonoBehaviour
         //m_nav_mesh_agent_.SetDestination()
     }
 
-    private void OnMouseHandler()
+    private void OnClickLeftMouseHandler()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
@@ -28,8 +31,18 @@ public class Controls : MonoBehaviour
             m_nav_mesh_agent_.SetDestination(hit.point);
     }
 
+    private void OnClickRightMouseHandler()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000))
+        this.m_current_selected_object_ = hit.transform.gameObject;
+    }
+
     private void OnDestroy()
     {
-        InputManager.GetInstance().OnClickLeftMouseButton-= OnMouseHandler;
+        InputManager.GetInstance().OnClickLeftMouseButton -= OnClickLeftMouseHandler;
+        InputManager.GetInstance().OnClickRightMouseButton -= OnClickRightMouseHandler;
+
     }
 }
