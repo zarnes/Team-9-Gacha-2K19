390f3dbeab362be04e1475440dabf89020fb4da8
Nedriia <arthur.favre@hotmail.com>
2019-04-14 02:03:39 +0200

    Procedural Map Generator

--- Assets/Scripts/RoomGenerator.cs
+++ Assets/Scripts/RoomGenerator.cs
@@ -6,6 +6,13 @@ public class RoomGenerator : MonoBehaviour
 {
     public GameObject RoomPrefab;
 
+    public GameObject floor;
+
+    public List<GameObject> barrels = new List<GameObject>();
+    public List<GameObject> walls = new List<GameObject>();
+    public int barrelsToSpawn;
+    public int wallsToSpawn;
+
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.M))
@@ -16,6 +23,38 @@ public class RoomGenerator : MonoBehaviour
 
     public void Generate()
     {
-        Instantiate(RoomPrefab);
+        for (int a = 0; a < barrelsToSpawn; a++)
+        {
+            var barrelsToSpawn = Random.Range(0, barrels.Count);
+            Debug.Log(barrelsToSpawn);
+            for (int i = 0; i < barrels.Count; i++)
+            {
+                if (barrelsToSpawn == i)
+                {
+                    var xPlane = Random.Range(-floor.GetComponent<Collider>().bounds.size.x / 2, floor.GetComponent<Collider>().bounds.size.x / 2);
+                    var zPlane = Random.Range(-floor.GetComponent<Collider>().bounds.size.z / 2, floor.GetComponent<Collider>().bounds.size.z / 2);
+                    Vector3 positionToSpawn = new Vector3(xPlane, floor.transform.localScale.y, zPlane);
+                    Debug.Log(positionToSpawn);
+                    Instantiate(barrels[i], positionToSpawn, Quaternion.identity);
+                }
+            }
+        }
+
+        for (int a = 0; a < wallsToSpawn; a++)
+        {
+            var barrelsToSpawn = Random.Range(0, walls.Count);
+            Debug.Log(barrelsToSpawn);
+            for (int i = 0; i < walls.Count; i++)
+            {
+                if (wallsToSpawn == i)
+                {
+                    var xPlane = Random.Range(-floor.GetComponent<Collider>().bounds.size.x / 2, floor.GetComponent<Collider>().bounds.size.x / 2);
+                    var zPlane = Random.Range(-floor.GetComponent<Collider>().bounds.size.z / 2, floor.GetComponent<Collider>().bounds.size.z / 2);
+                    Vector3 positionToSpawn = new Vector3(xPlane, floor.transform.localScale.y, zPlane);
+                    Debug.Log(positionToSpawn);
+                    Instantiate(walls[i], positionToSpawn, Quaternion.identity);
+                }
+            }
+        }
     }
 }
