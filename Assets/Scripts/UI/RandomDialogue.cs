61d48965ad3613afb1c56d025d8741d5b57c474a
HippDesf <3dil17.desfonds@ucolaval.net>
2019-04-14 23:03:14 +0200

    Dialogue terminée

---
+++ Assets/Scripts/UI/RandomDialogue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomDialogue : MonoBehaviour
+{
+    public List<string> randomDialogues = new List<string>();
+    public Bble_Text textScript;
+    IEnumerator dialogue;
+
+    private bool isActive = true;
+
+    void Start()
+    {
+        if (textScript == null)
+            textScript = GameObject.FindObjectOfType<Bble_Text>();
+
+
+        dialogue = Dialogue();
+        StartCoroutine(dialogue);
+    }
+
+    IEnumerator Dialogue()
+    {
+        while (isActive)
+        {
+            yield return new WaitForSeconds(Random.Range(0f, 20f));
+            Debug.Log("perform text");
+            textScript.gameObject.SetActive(true);
+            int index = Random.Range(0, randomDialogues.Count);
+            textScript.Setup(randomDialogues[index]);
+        }
+    }
+}
