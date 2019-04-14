e23047c5187f6e25ca55baad29b2f2769555fd0e
HippDesf <3dil17.desfonds@ucolaval.net>
2019-04-14 20:35:47 +0200

    adding prefab furnace

--- Assets/Scripts/UI/Bble_Text.cs
+++ Assets/Scripts/UI/Bble_Text.cs
@@ -6,8 +6,22 @@ using UnityEngine.UI;
 public class Bble_Text : MonoBehaviour
 {
     public Text txtRef;
+    public float rate = 0.1f;
+
+    IEnumerator displaytext;
 
     public void Setup(string txt) {
         txtRef.text = txt;
+    }
+
+    IEnumerator DisplayText(string txt)
+    {
+        int lenght = txt.Length;
+      //  string displayedString
+
+        for (int i = 0;i < lenght;i++)
+        {
+            yield return new WaitForSeconds(rate);
+        }
     }
 }
