b0edd59a3d36590443e6304c0aa4fec7206d857a
OlivierArgentieri <olivierargentieri@gmail.com>
2019-04-14 17:40:27 +0200

    Update Class pcikable

---
+++ Assets/Scripts/Pickable/Pickable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable : MonoBehaviour
+{
+    public Sprite m_sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
