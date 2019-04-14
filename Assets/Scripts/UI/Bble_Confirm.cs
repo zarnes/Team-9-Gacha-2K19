a06ff872aba013a932ac16436e63359c4e817b10
Guillaume <redtri@outlook.com>
2019-04-14 11:24:07 +0200

    UI - Confirm Bubble + Anchored

---
+++ Assets/Scripts/UI/Bble_Confirm.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bble_Confirm : MonoBehaviour
+{
+    public Image img;
+    public Text txtQty;
+    public Text txtDuration;
+    public Slider barRisk;
+
+    public void Setup(Sprite spr, string qty, string duration, float percent) {
+        img.sprite = spr;
+        txtQty.text = qty;
+        txtDuration.text = duration;
+        barRisk.value = percent;
+    }
+}
