6bca56944b392c2bc6f711bcb0f1fc6ca959f9d5
OlivierArgentieri <olivierargentieri@gmail.com>
2019-04-14 18:42:10 +0200

    update controls

--- Assets/Scripts/3C/Controls.cs
+++ Assets/Scripts/3C/Controls.cs
@@ -12,9 +12,13 @@ public class Controls : MonoBehaviour
 
     [SerializeField]
     private GameObject m_fire_camp;
-    
-    private GameObject m_destination_pickup_;
+
+    [SerializeField]
+    private GameObject m_pop_up_confirm;
+
+    private ItemHolder m_destination_pickup_;
     private GameObject m_current_selected_object_;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,11 +33,12 @@ public class Controls : MonoBehaviour
 
     private void OnClickLeftMouseHandler()
     {
+        /*
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         LayerMask mask = LayerMask.GetMask("Ground");
         if (Physics.Raycast(ray, out hit, 1000, mask))
-            m_nav_mesh_agent.SetDestination(hit.point);
+            m_nav_mesh_agent.SetDestination(hit.point);*/
     }
 
     private void OnClickRightMouseHandler()
@@ -45,14 +50,16 @@ public class Controls : MonoBehaviour
             return;
         }
 
-        var Item = GetGameObjectInRaycastAllByTag("Food");
+        // todo 
+        var Item = GetItem();
         if (Item == null)
-            Item = GetGameObjectInRaycastAllByTag("Wood");
+            Item = GetItem();
+
         if (m_current_selected_object_ != null && Item != null)
         {
-            // todo : Run ui condition
+            m_pop_up_confirm.GetComponent<AnchoredSpriteUI>().target = Item.transform;
             m_destination_pickup_ = Item;
-            m_nav_mesh_agent.SetDestination(Item.transform.position);
+            m_pop_up_confirm.SetActive(true);
             return;
         }
         this.m_current_selected_object_ = null;
@@ -78,16 +85,43 @@ public class Controls : MonoBehaviour
         return null;
     }
 
+    private ItemHolder GetItem()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit[] RayCastAll = Physics.RaycastAll(ray, 100);
+        ItemHolder item;
+        for (int i = 0; i < RayCastAll.Length; i++)
+        {
+            item = RayCastAll[i].transform.gameObject.GetComponent<ItemHolder>();
+            if (item != null)
+            {
+                return item;
+            }
+        }
+        return null;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.GetHashCode() == m_destination_pickup_.GetHashCode())
+        if (m_destination_pickup_ != null && other.gameObject.GetHashCode() == m_destination_pickup_.GetHashCode())
             ComeBackBackToCamp();
     }
 
     private void ComeBackBackToCamp()
     {
-        Vector3 RandomPositionComeBack = Random.insideUnitSphere;
-        RandomPositionComeBack += m_fire_camp.transform.position;
-        m_nav_mesh_agent.SetDestination(RandomPositionComeBack);
+        m_nav_mesh_agent.SetDestination(m_fire_camp.transform.position);
+    }
+
+    public void MoveToDestination()
+    {
+        if (m_destination_pickup_ != null)
+            m_nav_mesh_agent.SetDestination(m_destination_pickup_.transform.position);
+        m_pop_up_confirm.SetActive(false);
+    }
+
+    public void CancelMoveToDestination()
+    {
+        m_destination_pickup_ = null;
+        m_pop_up_confirm.SetActive(false);
     }
 }
--- Assets/Scripts/ItemHolder/ItemHolder.cs
+++ Assets/Scripts/ItemHolder/ItemHolder.cs
@@ -1,15 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pickable : MonoBehaviour
+public class ItemHolder : MonoBehaviour
 {
-    public Sprite m_sprite;
+    [SerializeField]
+    private Item m_item;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (m_item == null)
+            m_item = new WoodItem("Bois", 10);
+
     }
 
     // Update is called once per frame
